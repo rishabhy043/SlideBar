4742e566f5b3e66b60c27710800fbcb60f2e159a
rishabhy043 <rishabh.knipss043@gmail.com>
2024-11-24 20:09:36 +0530

    commit

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ let scrollbar = document.querySelector('.gallery');
 let backbtn = document.getElementById('backbtn')
 let nextbtn = document.getElementById('nextbtn')
 
+scrollbar.addEventListener("wheel" , (evt) => {
+  evt.preventDefault();
+scrollbar.scrollLeft += evt.deltaY;
+})
